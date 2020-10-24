a52da04163cee1bf227eedd4e733790a1cd60a99
haoranyu <yu.haora@northeastern.edu>
2020-10-24 17:38:45 -0400

    interface-in-typescript

---
+++ typescript_study/07-interfaces/Driver.ts
@@ -0,0 +1,15 @@
+import { Coach } from './Coach';
+import { CricketCoach } from './CricketCoach';
+import { GolfCoach } from './GolfCoach';
+
+let myCriketCoach = new CricketCoach();
+let myGolfCoach = new GolfCoach();
+
+let theCoaches: Coach[] = [];
+
+theCoaches.push(myCriketCoach);
+theCoaches.push(myGolfCoach);
+
+for (let tempCoach of theCoaches) {
+    console.log(tempCoach.getDailyWorkout());
+}
